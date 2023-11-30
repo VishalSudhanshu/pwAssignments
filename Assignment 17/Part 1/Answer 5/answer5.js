5b12dcb59f524ea1a0a1005584e5fdfea8c74ac9
VishalSudhanshu <vishalsudhanshu01@gmail.com>
2023-11-30 17:50:23 +0530

    Assignment 19

---
+++ Assignment 17/Part 1/Answer 5/answer5.js
@@ -0,0 +1,17 @@
+// Comments in JavaScript are like little notes you leave for yourself or others in your code. They don't affect the actual execution of the code but provide valuable information about what's going on. Here are examples of single-line and multi-line comments:
+
+// This is a single-line comment
+let a = 10; // You can also add comments at the end of a line of code
+
+/*Single-line comments are great for short explanations or clarifications. They start with // and continue until the end of the line.*/
+
+/*
+  This is a multi-line comment.
+  You can write more extensive comments or explanations here.
+  It's useful for providing context, documentation, or temporarily disabling code.
+*/
+let b = 20;
+
+// Multi-line comments are enclosed between /* and */. They are useful when you need to add more detailed explanations or disable a block of code temporarily.
+
+// Using comments effectively can make your code more understandable and maintainable, especially for yourself and others who might read your code in the future.
---
+++ Assignment 19/Answer 5/index.js
@@ -0,0 +1,14 @@
+const ascending = () =>{
+    for(let i = 1; i <= 25; i++){
+        console.log(i);
+    } 
+}
+
+const descending = () => {
+    for (let i = 25; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+descending()
+ascending()
